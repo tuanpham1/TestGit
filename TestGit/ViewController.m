7d5cb6bfceedf5b3350b33ff5a5b5b873723fe68
tuanpham <tuanpham@cnc.com.vn>
2013-06-04 16:07:16 +0700

    check out branch

--- TestGit/ViewController.m
+++ TestGit/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"This is a brach master");
+    NSLog(@"This is a brach Part 2");
 }
 
 - (void)didReceiveMemoryWarning
